42e55d5532c7b3dd3fdd88e9848681c42d9b328e
William Misener <wmisener@cs.uchicago.edu>
2016-03-06 17:15:32 -0600

    Good memes brah

--- dining_scraper.py
+++ dining_scraper.py
@@ -14,6 +14,12 @@ Brunch (S,U): 1881
 import bs4
 import requests
 
+def make_url(hall, meal):
+    halls = {"South": 1604, "Bartlett": 1630}
+    meals = {"Breakfast": 296, "Lunch": 297, "Dinner": 298, "Brunch": 1881}
+    url = "http://univofchicago.campusdish.com/Commerce/Catalog/Menus.aspx?LocationId={loc}&PeriodId={time}".format(loc = halls[hall], time = meals[meal])
+    return url
+
 def get_webpage(url, encoder):
     r = requests.get(url)
     r.encoding = encoder
@@ -21,7 +27,8 @@ def get_webpage(url, encoder):
     soup = bs4.BeautifulSoup(page, "html5lib")
     return soup
 
-def find_dining_menu_items(url):
+def find_dining_menu_items(hall, meal):
+    url = make_url(hall, meal)
     soup = get_webpage(url, "utf-8")
     items = soup.find_all('p', class_="mini freeze")
     menu = []
@@ -30,5 +37,11 @@ def find_dining_menu_items(url):
         menu.append(nicer_item)
     return menu
 
-def find_menupages_items(url):
-    pass
+def search_menu(menu, search_item):
+    return_list = []
+    for item in menu:
+        lc_item = item.lower()
+        lc_search = search_item.lower()
+        if lc_search in lc_item:
+            return_list.append(item)
+    return return_list
--- menu_scraper.py
+++ menu_scraper.py
@@ -1,9 +1,11 @@
 import requests
 import bs4
 import dining_scraper
+import re
 
-def scrape_rest():
-    url = "http://chicago.menupages.com/restaurants/the-snail/menu"
+def scrape_rest(rest_dict, rest):
+
+    url = "http://chicago.menupages.com{link}menu".format(link = rest_dict[rest][1])
     headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0' }
     r = requests.get(url, headers=headers)
     page = r.text
@@ -13,3 +15,28 @@ def scrape_rest():
     for item in items:
         menu.append(item.text)
     return menu
+
+def find_rest_list():
+    url = "http://chicago.menupages.com/restaurants/all-areas/hyde-park-kenwood/all-cuisines/"
+    headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0' }
+    r = requests.get(url, headers=headers)
+    page = r.text
+    soup = bs4.BeautifulSoup(page, "html5lib")
+    items = soup.find_all('td', class_='name-address')
+    rest_dict = {}
+    for item in items:
+        name = item.find('a').text
+        link = item.find('a').get('href')
+        add = re.search('\d\d\d.+\|', str(item.text))
+        if add:
+            add = add.group()[:-2]
+        rest_dict[add] = (name, link)
+    return rest_dict
+
+def tester(soup):
+    items = soup.find_all('td', class_='name-address')
+    print(items[0].find('a').text)
+    print(items[0].find('a').get('href'))
+    add = re.search('\d\d\d.+\|', str(items[0].text))
+    print(add.group()[:-2])
+
