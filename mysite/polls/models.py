335b1c2af6a729c65b540885ff238af62ea056b3
William Misener <wmisener@cs.uchicago.edu>
2016-03-07 23:43:50 -0600

    You want it you need it

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,8 @@
 from django import forms
-
+import sqlite3
+import sys
+sys.path.insert(0, '/home/student/cs122project')
+import yelpapi
 
 def Food_search(search_params):
     '''
@@ -22,12 +25,13 @@ def Food_search(search_params):
         for option in find_restaurants(address, cuisine):
             options.append(option)
 
-    if 'Food_trucks' in search_params:
-        for option in find_food_trucks(cuisine):
-            options.append(option)
+    #if 'Food_trucks' in search_params:
+    #    for option in find_restaurants(address, cuisine, type='f'):
+    #        options.append(option)
 
     if 'Dining_hall' in search_params:
-        for 
+        for option in []:
+            break
 
     return [food_option(search_params).params]
 
@@ -52,6 +56,8 @@ def find_restaurants(address, cuisine):
     Takes parameters and searches for restaurants matching the search criteria.
     Returns a list of food_option objects.
     '''
-    # take this out
-    return None
-    ### call yelp thing
+
+    yelpapi.yelp_search(address, term=cuisine)
+    #connection_yelp = sqlite3.connect("yelp_database")
+    #db = connection_yelp.cursor()
+
