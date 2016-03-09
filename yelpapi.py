62ac52154f0959d335165009c104db9a7ff7eb04
Russell Bailey <rubailey@cs.uchicago.edu>
2016-03-09 00:05:39 -0600

    that aint yelp

--- yelpapi.py
+++ yelpapi.py
@@ -37,7 +37,7 @@ def yelp_search(location, term=None, coordinates=False):
         results = client.search(location, **params)
 
     #drop_yelp_table("yelp_results")
-    table_string = "CREATE TABLE yelp_results (name varchar(50), latitude int, longitude REAL, rating real, address varchar(50), city varchar(25), state varchar(3), zip_code integer, phone_number integer, distance varchar(10), walking_time varchar(10));"
+    table_string = "CREATE TABLE yelp_results (name varchar(50), latitude real, longitude real, rating real, address varchar(50), city varchar(25), state varchar(3), zip_code integer, phone_number integer, distance varchar(10), walking_time int) ;"
 
     connection_yelp = sqlite3.connect("yelp_database")
     db = connection_yelp.cursor()
@@ -47,6 +47,10 @@ def yelp_search(location, term=None, coordinates=False):
         
         if not(results.businesses[i].location.coordinate == None or results.businesses[i].location.address == []):
             distance, walking_time = get_distance(location, results.businesses[i].location.address[0] +", "+ results.businesses[i].location.city)
+            if not 'hour' in walking_time:
+                walking_time = int(walking_time.split()[0])
+            else:
+                walking_time = 60*int(walking_time.split()[0]) + int(walking_time.split()[2])
             one_line = [results.businesses[i].name, str(results.businesses[i].location.coordinate.latitude), str(results.businesses[i].location.coordinate.longitude), str(results.businesses[i].rating), results.businesses[i].location.address[0], results.businesses[i].location.city, results.businesses[i].location.state_code, results.businesses[i].location.postal_code,results.businesses[i].phone, distance, walking_time]
             db.execute("Insert into yelp_results Values (?,?,?,?,?,?,?,?,?,?,?);", one_line)
             
@@ -65,21 +69,25 @@ def yelp_search_food_trucks(location, term=None):
     params = {"category_filter":"foodtrucks"}
     if not term == None:
         params["term"] = term
-    print (params)
 
     results = client.search("Chicago", **params)
 
-    #drop_yelp_table("yelp_food_trucks")
-    table_string = "CREATE TABLE yelp_food_trucks (name varchar(50), rating real, phone_number integer, distance varchar(10), walking_time varchar(10), arrive_time varchar(5), leave_time varchar(5));"
+    drop_yelp_table("yelp_food_trucks")
+    table_string = "CREATE TABLE yelp_food_trucks (name varchar(50), rating real, phone_number integer, distance varchar(10), walking_time int, arrive_time varchar(5), leave_time varchar(5));"
 
     connection_yelp = sqlite3.connect("yelp_database")
     db = connection_yelp.cursor()
     db.execute(table_string)
 
+    print (results)
     #all food trucks are on near 58th and University
     distance, walking_time = get_distance(location, "5800 University, Chicago, IL")
+    if not 'hour' in walking_time:
+        walking_time = int(walking_time.split()[0])
+    else:
+        walking_time = 60*int(walking_time.split()[0]) + int(walking_time.split()[2])
     trucks_today, truck_dict = scrap_food_trucks()
-    print(trucks_today)
+    
     for i in range(len(results.businesses)):
         print (results.businesses[i].name)
         key = reasonable_permutation(results.businesses[i].name, trucks_today)
